6fe555a058fc9a2168076f2007ca5e2b52a87a84
Daniel Desktop <d.oconnor122@gmail.com>
2018-03-02 15:02:26 +0000

    up

--- server.js
+++ server.js
@@ -107,8 +107,10 @@ con2.query(sql22, function(err, RESULT2) {
 
 function Process(RESULT2){
 
-console.log("Results are" + RESULT2);
-console.log(JSON.parse(RESULT2));
+var RefRel = JSON.stringify(RESULT2);
+console.log(JSON.parse("user", RefRel));
+
+
 }
 
 InitialQ(Process);
